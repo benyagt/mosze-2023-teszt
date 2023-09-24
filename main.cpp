f507534558b141ee9f551e1536d6fa00debb6f53
benyagt <benisolyom@outlook.hu>
2023-09-24 16:20:39 +0200

    kommenteltem a hibakat

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //N_ELEMENTS
+    std::cout << '1-100 ertekek duplazasa' //hianyzo sorzaro pontosvesszo, idezojel kell aposztrof helyett
+    for (int i = 0;)  //hianyzik sokminden a ciklusbol
     {
-        b[i] = i * 2;
+        b[i] = i * 2; //(i+1)-et kellett volna irni
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //hibas ciklus
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //hianyzo ertek meg endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; //memoriaszemet van bene
+    for (int i = 0; i < N_ELEMENTS, i++) //vesszo helyett pontos vesszo
     {
-        atlag += b[i]
+        atlag += b[i] //pontosvesszo!!
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
